56fb9ea85ee3d51ce2901d6c40df691499ec43f7
robin-yuan <bin924@126.com>
2018-04-08 16:43:06 +0800

    Update robot.js

--- robot.js
+++ robot.js
@@ -201,10 +201,8 @@
         sendMove10("set_drive_loop", duration, callback);
     };
      ext.connect = function() {
-      ws = new WebSocket('ws://localhost:8080');
-         
-     ws.onopen = function()
-     { ws.send(“hello”);}
+      ws = new WebSocket('ws://localhost:8080');   
+     ws.send(“hello”) ;
       ws.onmessage = function(evt) {
         data = JSON.parse(evt.data);
         sensors_depth = data.depth_adc ;
