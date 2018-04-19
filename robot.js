c375e2be5326a98e34fd89c57008c7dacbcf9b2e
robin-yuan <bin924@126.com>
2018-04-19 15:34:30 +0800

    Update robot.js

--- robot.js
+++ robot.js
@@ -3,7 +3,7 @@
 "use strict";
 (function (ext) {
     // Default step duration: 0.5s
-    var sensors_depth = 0;
+    var sensors_depth = 1;
     var sensors_yaw = 0 ;
     var sensors_pitch = 0;
     var sensors_battery = 0;
@@ -29,8 +29,8 @@
             xmlHttp.onreadystatechange = function () {
                 if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
                    //return xmlHttp.responseText;
-                  /*    callback(xmlHttp.responseText); */
-                   sensors_depth = xmlHttp.responseText ;
+                      callback(xmlHttp.responseText); 
+                 //  sensors_depth = xmlHttp.responseText ;
                    // return sensors_depth;
                 }
             };
@@ -217,10 +217,10 @@
            xmlHttp1.send(null);
            xmlHttp1.onreadystatechange = function () {
                 if (xmlHttp1.readyState == 4 && xmlHttp1.status == 200) {
-                    // sensors_depth = xmlHttp.responseText ;
-                    var data = eval('('+xmlHttp.responseText+')') ;
-                     sensors_depth = data.rov.depth ;
-                     sensors_yaw = data.rov.yaw;
+                     sensors_depth = xmlHttp.responseText ;
+                 var data = eval('('+xmlHttp.responseText+')')  ;
+                    // sensors_depth = data.rov.depth ;
+                     sensors_yaw = 12;
                      sensors_pitch = data.rov.pitch;
                      sensors_battery = data.rov.battery;
                      sensors_temp = data.rov.temp;
